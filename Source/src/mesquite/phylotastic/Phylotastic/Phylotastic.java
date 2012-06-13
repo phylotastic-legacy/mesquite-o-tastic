be46190d0aacf03db6e7cdae39d6b02e7155d9f6
Peter Midford <peter.midford@gmail.com>
2012-06-13 13:50:53 -0600

    code cleanup, no new functionality

--- Source/src/mesquite/phylotastic/Phylotastic/Phylotastic.java
+++ Source/src/mesquite/phylotastic/Phylotastic/Phylotastic.java
@@ -15,6 +15,7 @@ import java.io.InputStreamReader;
 import mesquite.Mesquite;
 import mesquite.lib.Debugg;
 import mesquite.lib.EmployeeNeed;
+import mesquite.lib.MesquiteMessage;
 import mesquite.lib.MesquiteModule;
 import mesquite.lib.Taxa;
 import mesquite.lib.Tree;
@@ -30,12 +31,13 @@ public class Phylotastic extends ExternalTreeSearcher {
     String requestStr;
     
     final static String prefix = "http://phylotastic-wg.nescent.org/script/phylotastic.cgi?species=";
+    final static String FORMATSPECIFIER = "&format=";
+    final static String TREESPECIFIER = "&tree=";
     final static String suffix = "&tree=mammals&format=newick";
     final static String testString = 
         "http://phylotastic-wg.nescent.org/script/phylotastic.cgi?species=Erinaceus_europaeus,Nandinia_binotata,Manis_crassicaudata,Manis_javanica,Manis_pentadactyla&tree=mammals&format=newick";
-    
-//    final static String testTreeReturn =
-//        "(((Nandinia_binotata,((Manis_pentadactyla,Manis_crassicaudata),Manis_javanica))),Erinaceus_europaeus);";
+    final static String testFormat = "newick";
+    final static String testTree = "mammals";
     
     public void getEmployeeNeeds(){  //This gets called on startup to harvest information; override this and inside, call registerEmployeeNeed
     }
@@ -51,22 +53,29 @@ public class Phylotastic extends ExternalTreeSearcher {
         else {
             Debugg.println("Element of Taxas not a taxa block");
         }
-        requestStr = buildRequest(requestedTaxa);
+        String requestedFormat = testFormat;
+        String requestedTree = testTree;
+        requestStr = buildRequest(requestedTaxa,requestedFormat,requestedTree);
         Debugg.println("requestStr is " + requestStr);
         return true;
     }
 
     
-    private String buildRequest(Taxa taxa){
-        StringBuilder taxaString = new StringBuilder(200);
+    private String buildRequest(Taxa taxa, String formatId, String treeId){
+        StringBuilder requestString = new StringBuilder(2000);
+        requestString.append(prefix);
         for (int i = 0; i<taxa.getNumTaxa();i++){
             String rawName = taxa.getTaxonName(i);
             String newName = rawName.replace(' ','_');
-            taxaString.append(newName);
+            requestString.append(newName);
             if (i<taxa.getNumTaxa()-1)
-                taxaString.append(',');
+                requestString.append(',');
         }
-        return prefix+taxaString.toString()+suffix;
+        requestString.append(TREESPECIFIER);
+        requestString.append(treeId);
+        requestString.append(FORMATSPECIFIER);
+        requestString.append(formatId);
+        return requestString.toString();
     }
 
     public Class getDutyClass() {
@@ -89,20 +98,19 @@ public class Phylotastic extends ExternalTreeSearcher {
         return "Get tree using Phylotastic web services";
     }
 
-
+    //This method should be used to set the taxa block to build the query, but it is never called
     public void initialize(Taxa taxa) {
         Debugg.println("Entering initialize: " + taxa);
     }
 
 
     public void fillTreeBlock(TreeVector treeList) {
-        Debugg.println("Entering fillTreeBlock");
         String requestReturn;
         try {
             URL u = new URL(requestStr);
-            InputStream us = u.openStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(us));
-            StringBuilder treeStrBuilder = new StringBuilder(200);
+            final InputStream us = u.openStream();
+            final BufferedReader br = new BufferedReader(new InputStreamReader(us));
+            final StringBuilder treeStrBuilder = new StringBuilder(200);
             String raw = br.readLine();
             while(raw != null){
                 treeStrBuilder.append(raw);
@@ -112,8 +120,7 @@ public class Phylotastic extends ExternalTreeSearcher {
             requestReturn = treeStrBuilder.toString();
             
         } catch (Exception e) {
-            Debugg.println("URL problem: " + requestStr);
-            // TODO Auto-generated catch block
+            MesquiteMessage.warnProgrammer("URL for Phylotastic has a syntax problem; String is : " + requestStr);
             e.printStackTrace();
             return;
         }
